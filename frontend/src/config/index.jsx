d22fadef7d8d69cb8e1aba679e7c9d007c5f17f9
Kabir Singh <kabir.singh66564@example.com>
2025-11-15 00:22:05 +0530

    VerSion 1:Ready for Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}=require("axios");
 
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL="https://proconnect-for-jobseekers.onrender.com"
 export const clientServer=axios.create(
     {
         baseURL:BASE_URL,
